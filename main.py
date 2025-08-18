a9d469c3ee2984c76d49d793853a8130215363b5
ve77on <vetton2017@gmail.com>
2025-08-18 15:35:32 +0300

    Task

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
+# #
+# # Создайте класс Car с атрибутами make, model и year. Добавьте метод display_info(),
+# # который выводит информацию о машине. Затем создайте объект этого класса и вызовите метод display_info().
+# # Требования:
+# # •	Программа должна создавать класс с названием Car, который содержит атрибуты make, model и year.
+# # •	Класс Car должен содержать метод display_info(), который выводит информацию о машине.
+# # •	Программа должна создавать объект класса Car.
+# # •	Программа должна вызывать метод display_info() для объекта класса Car.
 #
-# Создайте класс Car с атрибутами make, model и year. Добавьте метод display_info(),
-# который выводит информацию о машине. Затем создайте объект этого класса и вызовите метод display_info().
+#
+# class Car:
+#     def __init__(self,make,model,year):
+#         self.make=make
+#         self.model = model
+#         self.year = year
+#
+#     def display_info(self):
+#         print(f"Марка: {self.make}, model: {self.model}, year: {self.year}")
+#
+#
+# car1 = Car("Toyota", "Camry", 2015)
+#
+# car1.display_info()
+
+
+# Создайте класс Library с атрибутом books, который представляет собой список книг.
+# Добавьте методы add_book(book) для добавления книги в библиотеку и display_books() для вывода списка всех книг.
+# Создайте объект класса Library, добавьте несколько книг и выведите список книг, используя методы объекта.
 # Требования:
-# •	Программа должна создавать класс с названием Car, который содержит атрибуты make, model и year.
-# •	Класс Car должен содержать метод display_info(), который выводит информацию о машине.
-# •	Программа должна создавать объект класса Car.
-# •	Программа должна вызывать метод display_info() для объекта класса Car.
+# •	Программа должна включать класс Library с атрибутом books, представляющим собой список книг.
+# •	Класс Library должен содержать метод add_book(book), который добавляет книгу в список books.
+# •	Класс Library должен содержать метод display_books(), который выводит список всех книг.
+# •	Программа должна создавать объект класса Library.
+# •	Программа должна использовать методы add_book(book) и display_books() на созданном объекте для добавления книг и вывода списка книг.
 
 
-class Car:
-    def __init__(self,make,model,year):
-        self.make=make
-        self.model = model
-        self.year = year
+class Library:
+    def __init__(self, books):
+        self.books = books
 
-    def display_info(self):
-        print(f"Марка: {self.make}, model: {self.model}, year: {self.year}")
+    def add_book(self, name_book):
+        self.books.append(name_book)
+
+    def display_books(self):
+        print(f"Books: {self.books}")
 
 
-car1 = Car("Toyota", "Camry", 2015)
+collection = Library([])
 
-car1.display_info()
+collection.add_book("Harry Potter")
+collection.display_books()
+
+
+
+
+
+
+
 
 
